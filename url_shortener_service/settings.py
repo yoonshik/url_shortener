daa23dfa09ed876a52205b5016823b1bff4ca744
yoonshik <me@yoonshik.com>
2022-09-20 17:43:42 -0400

    Add database configuration

--- url_shortener_service/settings.py
+++ url_shortener_service/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'url_shortener_service.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.environ.get('DATABASE_NAME', ''),
+        'USER': os.environ.get('DATABASE_USER', ''),
+        'PASSWORD': os.environ.get('DATABASE_PASSWORD', ''),
+        'HOST': os.environ.get('DATABASE_URL', ''),
+        'PORT': os.environ.get('DATABASE_PORT', ''),
     }
 }
 
