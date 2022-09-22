fc41d6a6f79e70d434350547873cbf1daea40ad8
yoonshik <me@yoonshik.com>
2022-09-22 12:45:02 -0400

    min length 1 path

--- url_creator/shortened_url_generator.py
+++ url_creator/shortened_url_generator.py
@@ -9,7 +9,7 @@ def generate_url_paths(count):
     os.environ.get('DATABASE_PORT', '')
     url_paths = []
     for _ in range(count):
-        n = random.randint(0, int(MAX_URL_PATH_LENGTH))
+        n = random.randint(1, int(MAX_URL_PATH_LENGTH))
         url_paths.append(generate_random_string(n))
     return url_paths
 
